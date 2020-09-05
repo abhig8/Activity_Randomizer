700a420053737aae5734ddcb76a486e59c7d8cd7
Neetu Gupta <nee_g@Neetus-MBP.attlocal.net>
2020-09-04 22:42:34 -0700

    Activity Update #1

--- quarantineActivity/activities.swift
+++ quarantineActivity/activities.swift
@@ -10,17 +10,19 @@ import UIKit
 
 class activities {
     
+    // modify to use dictionaries with links and/or photos attached to each activity for more engaging user experience
+    
     var activityType: String
         
-    let exercise = ["10 pushups", "10 squats", "10 pull-ups", "Run 1 mile"]
+    let exercise = ["10 pushups", "10 squats", "10 pull-ups", "Run 1 mile", "10 dips", "10 rows", "monkey bars"]
     
     let cooking = ["Pancakes", "Whipped Coffee", "Fries", "Samosas"]
     
-    let tvShow = ["The Flash", "Spongebob", "Disney", "Fairly Odd Parents"]
+    let tvShow = ["The Flash", "Spongebob", "Disney", "Fairly Odd Parents", "randomize on Netflix"]
     
-    let learn = ["Try a new language", "Try a new instrument", "Try a new sport", "Practice your favorite subject"]
+    let learn = ["Try a new language", "Try a new instrument", "Try a new sport", "Practice your favorite subject", "Learn a programming language", "Learn how to cook", "Do an online course"]
     
-    let socialize = ["Snapchat", "Instagram", "Facebook", "Messages"]
+    let socialize = ["Snapchat", "Instagram", "Facebook", "Messages", "Messenger", "Whatsapp"]
     
     let reading = ["Harry Potter", "Percy Jackson", "New York Times", "Your favorite Blog"]
     
